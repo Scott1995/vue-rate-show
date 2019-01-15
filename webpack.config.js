4348d3958eee8f7111f8447b5fd788c54f235cb6
这么大的玉米！ <15157698793@163.com>
2019-01-15 10:32:00 +0800

    /

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  // entry: './src/main.js',
-  entry: './src/lib/index.js',
+  entry: './src/main.js',
+  // entry: './src/lib/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    // filename: 'build.js',
-    filename: 'vue-rate-show.js',
+    filename: 'build.js',
+    // filename: 'vue-rate-show.js',
     // library: 'RateShow', // library指定的就是你使用require时的模块名，这里便是require("RateShow")
     // libraryTarget: 'umd', //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     // umdNamedDefine: true
